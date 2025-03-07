dcf929d1d95cb343320302c28f86ea022b8f19c1
Nikolaos93 <jelic.nikolaa@gmail.com>
2025-03-07 23:58:34 +0100

    Implemented a prototype for Credits screen
    - Made new canvas with image and text for displaying Credits
    - Implemented Credits.cs script to make the Credits text scroll

---
+++ Assets/Scripts/Credits.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+    public float scrollSpeed = 40.0f;
+
+    private RectTransform rectTransform;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get the RectTransform component of the UI element
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the text upwards over time
+        rectTransform.anchoredPosition += new Vector2(0, scrollSpeed * Time.deltaTime);
+    }
+}
